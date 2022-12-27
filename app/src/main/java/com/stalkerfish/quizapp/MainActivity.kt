27e00eaec7ab8dc15a34056d54a593cadfda2c91
Allison Silva <silva.allison2002@gmail.com>
2022-12-27 07:15:33 -0300

    feat: add the custom Toast and hooks to the QuestionsActivity it does not allow the user to enter the Quiz if they don't input a username

--- app/src/main/java/com/stalkerfish/quizapp/MainActivity.kt
+++ app/src/main/java/com/stalkerfish/quizapp/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.stalkerfish.quizapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.core.content.res.ResourcesCompat
+import www.sanju.motiontoast.MotionToast
+import www.sanju.motiontoast.MotionToastStyle
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val startButton: Button = findViewById(R.id.startBtn)
+        val personName: EditText = findViewById(R.id.person_name)
+
+        startButton.setOnClickListener {
+
+            if (personName.text.isEmpty()){
+                MotionToast.darkColorToast(this, null,"Cannot start without your name!",
+                    MotionToastStyle.ERROR,
+                    MotionToast.GRAVITY_BOTTOM,
+                    MotionToast.LONG_DURATION,
+                    ResourcesCompat.getFont(this, www.sanju.motiontoast.R.font.helvetica_regular))
+            }
+            else{
+                val intent = Intent(this, QuestionsActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
     }
 }
