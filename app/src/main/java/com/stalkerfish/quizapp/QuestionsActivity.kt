3b3961b3163071c288b01d31db5463e8cffc8a93
Allison Silva <silva.allison2002@gmail.com>
2022-12-29 10:50:41 -0300

    refactor: add IDs and invoke them in the activity class

--- app/src/main/java/com/stalkerfish/quizapp/QuestionsActivity.kt
+++ app/src/main/java/com/stalkerfish/quizapp/QuestionsActivity.kt
@@ -3,11 +3,38 @@ package com.stalkerfish.quizapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
 
 class QuestionsActivity : AppCompatActivity() {
+
+    private var questionText: TextView? = null
+    private var questionImage: ImageView? = null
+    private var progressBar: ProgressBar? = null
+    private var progressText: TextView? = null
+
+    private var answer1: TextView? = null
+    private var answer2: TextView? = null
+    private var answer3: TextView? = null
+    private var answer4: TextView? = null
+
+    private var submitBtn: Button? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_questions)
+
+        questionText = findViewById(R.id.question_text)
+        questionImage = findViewById(R.id.flag)
+        progressBar = findViewById(R.id.progress_bar)
+        progressText = findViewById(R.id.progress_text)
+
+        answer1 = findViewById(R.id.answer_1)
+        answer2 = findViewById(R.id.answer_2)
+        answer3 = findViewById(R.id.answer_3)
+        answer4 = findViewById(R.id.answer_4)
 
         val questionsList = Constants.getQuestions()
         Log.i("QuestionsList size is", "${questionsList.size}")
