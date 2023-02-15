ff50f16f68dafa2e119df5687a95fbdefc37ea61
Allison Silva <silva.allison2002@gmail.com>
2023-02-15 11:48:01 -0300

    feat: TextViews act like buttons

--- app/src/main/java/com/stalkerfish/quizapp/QuestionsActivity.kt
+++ app/src/main/java/com/stalkerfish/quizapp/QuestionsActivity.kt
@@ -1,14 +1,21 @@
 package com.stalkerfish.quizapp
 
+import android.graphics.Color
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 
-class QuestionsActivity : AppCompatActivity() {
+class QuestionsActivity : AppCompatActivity(), View.OnClickListener {
+
+    private var mCurrentPosition: Int = 1
+    private var mQuestionsList: ArrayList<Question>? = null
+    private var mSelectedOptionPosition: Int = 1
 
     private var questionText: TextView? = null
     private var questionImage: ImageView? = null
@@ -36,20 +43,87 @@ class QuestionsActivity : AppCompatActivity() {
         answer3 = findViewById(R.id.answer_3)
         answer4 = findViewById(R.id.answer_4)
 
-        val questionsList = Constants.getQuestions()
+        answer1?.setOnClickListener(this)
+        answer2?.setOnClickListener(this)
+        answer3?.setOnClickListener(this)
+        answer4?.setOnClickListener(this)
 
-        var currentPosition = 1
-        val question: Question = questionsList[currentPosition - 1]
+        mQuestionsList = Constants.getQuestions()
+
+        setQuestion()
+
+    }
+
+    private fun defaultOptionsView(){
+        val options = ArrayList<TextView>()
+        answer1?.let {
+            options.add(0, it)
+        }
+        answer2?.let {
+            options.add(1, it)
+        }
+        answer3?.let {
+            options.add(2, it)
+        }
+        answer4?.let {
+            options.add(3, it)
+        }
+
+        for(option in options){
+            option.setTextColor(Color.WHITE)
+            option.typeface = Typeface.DEFAULT
+            option.background = ContextCompat.getDrawable(this, R.drawable.border_bg)
+        }
+
+    }
+
+    private fun selectedOptionsView(option: TextView, optionNumber: Int){
+        defaultOptionsView()
+
+        mSelectedOptionPosition = optionNumber
+
+        option.setTextColor(Color.parseColor("#363A43"))
+        option.setTypeface(option.typeface, Typeface.BOLD)
+        option.background = ContextCompat.getDrawable(this, R.drawable.selected_border_bg)
+    }
+
+    private fun setQuestion() {
+
+        val question: Question = mQuestionsList!![mCurrentPosition - 1]
 
         questionText?.text = question.question
         questionImage?.setImageResource(question.image)
 
-        progressBar?.progress = currentPosition
-        progressText?.text = "${currentPosition}/${progressBar?.max}"
-        
+        progressBar?.progress = mCurrentPosition
+        progressText?.text = "${mCurrentPosition}/${progressBar?.max}"
+
         answer1?.text = question.optionOne
         answer2?.text = question.optionTwo
         answer3?.text = question.optionThree
         answer4?.text = question.optionFour
+
+        if (mCurrentPosition == mQuestionsList!!.size){
+            submitBtn?.text = "FINISH"
+
+        }else{
+            submitBtn?.text = "SUBMIT"
+        }
+    }
+
+    override fun onClick(view: View?) {
+       when(view?.id){
+            R.id.answer_1 -> answer1?.let {
+                selectedOptionsView(it, 1)
+            }
+           R.id.answer_2 -> answer2?.let {
+                selectedOptionsView(it, 2)
+            }
+           R.id.answer_3 -> answer3?.let {
+                selectedOptionsView(it, 3)
+            }
+           R.id.answer_4 -> answer4?.let {
+                selectedOptionsView(it, 4)
+            }
+       }
     }
 }
